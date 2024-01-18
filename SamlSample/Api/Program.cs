e1e9c6790e2a1d4f26f5515de833969f399de48d
soags <17847282+soags@users.noreply.github.com>
2024-01-18 17:30:55 +0900

    テストログインページを追加

--- SamlSample/Api/Program.cs
+++ SamlSample/Api/Program.cs
@@ -3,5 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+var api = app.MapGroup("api");
+
+api.MapPost("login", (Credentials credentials) =>
+{
+    var response = new Response(credentials.Username);
+    return Results.Ok(response);
+});
 
 app.Run();
+
+
+record Credentials(string Username, string Password);
+
+record Response(string Username);
