3005dfc6b712a3a84dc04fb9b99d25824c4c7210
soags <17847282+soags@users.noreply.github.com>
2024-01-19 14:44:59 +0900

    OIDC認証

--- SamlSample/Api/Program.cs
+++ SamlSample/Api/Program.cs
@@ -1,23 +1,48 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:8080/auth/realms/myrealm"; // KeycloakのURL
+        options.Audience = "test-oidc"; // KeycloakのクライアントID
+        options.RequireHttpsMetadata = false;
+
+        options.Events = new JwtBearerEvents
+        {
+            OnChallenge = async context =>
+            {
+                context.HandleResponse();
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(JsonSerializer.Serialize(new
+                {
+                    context.ErrorDescription,
+                    context.Error,
+                }));
+            },
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-app.UseDefaultFiles();
+
 app.UseStaticFiles();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
-var api = app.MapGroup("api");
-
-api.MapPost("login", (Credentials credentials) =>
-{
-    var response = new Response(credentials.Username);
-    return Results.Ok(response);
-});
+app.MapControllers();
 
 app.Run();
-
-
-record Credentials(string Username, string Password);
-
-record Response(string Username);
